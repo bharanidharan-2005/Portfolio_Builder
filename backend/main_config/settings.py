a005a9629455a5a108c7b14a64cc35dc1be4d2d5
BHARANIDHARAN.M <laughinggupta2025@gmail.com>
2026-07-22 16:27:55 +0530

    Change email backend to SMTP for email configuration

--- backend/main_config/settings.py
+++ backend/main_config/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main_config.wsgi.application'
 
 # --- EMAIL SMTP CONFIGURATION ---
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
